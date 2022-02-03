5c53d245916d0443e3bd0539dff4f4e4c623e826
Tushar <s.v.tushar04@gmail.com>
2022-02-03 20:18:06 +0530

    modfied

--- TestApi.py
+++ TestApi.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import Test
 app=Flask(__name__)
+@app.route("/")
+def default():
+    return ({"Message":"Welcome to Cash flow manager api"})
 @app.route("/signup" ,methods = ['POST'])
 def signup():
     #Test.signup()
