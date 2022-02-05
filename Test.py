f994598ed25a398a08b41ecbe21d587f57d949bc
Tushar <s.v.tushar04@gmail.com>
2022-02-05 14:47:22 +0530

    Testing

--- Test.py
+++ Test.py
@@ -1,15 +1,13 @@
-from http import client
-import ssl
 import pymongo
 
 
 client = pymongo.MongoClient("mongodb+srv://tushar_v04:3d0fMui38rF3XHLD@cashflowmanagement.hwbys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-print(client)
 db=client["CashFlowManager"]
 userData=db["UserData"]
 
 def signup(user):
     userData.insert_one(user)
     userinfo=userData.find_one(user)
-    userid=userinfo["_id"]
+    userid=str(userinfo["_id"])
+    print(userid)
     return userid
