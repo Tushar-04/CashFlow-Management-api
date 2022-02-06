6457141eefe2c20af2ce3a35be9a456f38d28f6c
Tushar <s.v.tushar04@gmail.com>
2022-02-06 23:39:46 +0530

    bill

--- Test.py
+++ Test.py
@@ -1,20 +1,27 @@
 import pymongo
 
-
 client = pymongo.MongoClient("mongodb+srv://tushar_v04:3d0fMui38rF3XHLD@cashflowmanagement.hwbys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db=client["CashFlowManager"]
 userData=db["UserData"]
+billData=db["billData"]
+
 
 def signup(user):
+    userinfo=userData.find_one({"email":user["email"]})
+    if(userinfo !=None):
+        res={"message":"Email already exists","id":None}
+        return res
     userData.insert_one(user)
     userinfo=userData.find_one(user)
     userid=str(userinfo["_id"])
-    print(userid)
-    return userid
+    res={"message":"Sign up successful","id":userid}
+    return res
+
+
 def login(user):
     userinfo=userData.find_one({"email":user["email"]})
     if(userinfo==None):
-        res={"message":"User does'exit","id":None}
+        res={"message":"User does'exists","id":None}
     elif(user["password"]!=userinfo["password"]):
         res={"message":"Wrong password","id":None}
     elif(user["password"]==userinfo["password"]):
@@ -23,3 +30,9 @@ def login(user):
         res={"message":"somthing went wrong plz try again","id":None}
     
     return res
+
+def bill(billinfo):
+    billData.insert_one(billinfo)
+    info=billData.find_one(billinfo)
+    billid=str(info["_id"])
+    return ({"message":"Bill entered","id":billid})
--- Testapi.py
+++ Testapi.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import Test
+
 app=Flask(__name__)
+
 @app.route("/",methods = ['GET'])
 def default():
     return ({"message":"Welcome to Cash flow manager api"})
@@ -8,12 +10,18 @@ def default():
 @app.route("/signup" ,methods = ['POST'])
 def signup():
     user=dict(request.json)
-    userid=Test.signup(user)
-    res={"message":"Sign up successful","id":userid}
+    res=Test.signup(user)
     return res
+
 @app.route("/login" ,methods = ['POST'])
 def login():
     res=Test.login(request.json)
     return res
+
+@app.route("/bill" ,methods = ['POST'])
+def bill():
+    res=Test.bill(request.json)
+    return res
+
 if __name__ == '__main__':
     app.run(debug=True)
