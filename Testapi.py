4dda70403ed313fb3fadec29692181593506e9a8
Tushar <s.v.tushar04@gmail.com>
2022-02-08 21:50:15 +0530

    jsonfiy

--- Testapi.py
+++ Testapi.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
 import Test
 from flask_httpauth import HTTPBasicAuth
 
@@ -17,39 +17,39 @@ def verify(username,password):
 @app.route("/",methods = ['GET'])
 @auth.login_required
 def default():
-    return ({"message":"Welcome to Cash flow manager api"})
+    return jsonify({"message":"Welcome to Cash flow manager api"})
     
 @app.route("/signup" ,methods = ['POST'])
 @auth.login_required
 def signup():
     user=dict(request.json)
     res=Test.signup(user)
-    return res
+    return jsonify(res)
 
 @app.route("/login" ,methods = ['POST'])
 @auth.login_required
 def login():
     res=Test.login(request.json)
-    return res
+    return jsonify(res)
 
 @app.route("/bill" ,methods = ['POST'])
 @auth.login_required
 def bill():
     res=Test.bill(request.json)
-    return res
+    return jsonify(res)
 
 @app.route("/subscription" ,methods = ['POST'])
 @auth.login_required
 def subscription():
     res=Test.subscription(request.json)
-    return res
+    return jsonify(res)
 
 @app.route("/getuser" ,methods = ['GET'])
 @auth.login_required
 def getuser():
     uid=request.args.get("id")
     res=Test.getuser(uid)
-    return res
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True)
